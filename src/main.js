10a8feecfbac77c1e9020ed39693e0c6117a6c4a
onMarch19 <2797235541@qq.com>
2022-09-12 00:27:21 +0800

    import plugins

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+// 引入插件，放在此处引用是为了下面的router和store中可能会用到某些插件的功能。
+import './plugins/index'
+
 import router from './router'
 import store from './store'
 
