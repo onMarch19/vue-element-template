f7071759175c550502e9193a6590e63e7f0b80a1
onMarch19 <2797235541@qq.com>
2022-12-03 21:20:27 +0800

    add app-main

---
+++ src/views/layout/app-main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-main">
+    <transition name="fade-transform" mode="out-in">
+      <keep-alive :include="cachedViews">
+        <router-view :key="key" />
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppMain',
+  data() {
+    return {
+      cachedViews: []
+    }
+  },
+  computed: {
+    key() {
+      return this.$route.path
+    }
+  }
+}
+</script>
